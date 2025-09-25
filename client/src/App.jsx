34b691783efabb320018ac1e1d4ce459831a0739
Mridul Pandey <pandeym891@gmail.com>
2025-09-25 15:34:47 +0530

    initialisation of the project

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen border '> 
+        Hello 
+      </div>
+    </>
+  )
+}
+
+export default App
