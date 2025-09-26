43e8ec3d83e09b9b599d2e10a97d52a422f038a7
Mridul Pandey <pandeym891@gmail.com>
2025-09-26 23:28:07 +0530

    Creation of auth Middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+const authMiddleware = async (req, res, next) => {
+  const token = await req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    res.status(401).json({ message: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
+
+module.exports = { authMiddleware };
